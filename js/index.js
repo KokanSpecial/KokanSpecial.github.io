2ebb9de0ece5ccbad3021467e19be9572b182104
tejas <tejashatle2@gmail.com>
2020-12-24 10:02:31 +0530

    added price dropdown

---
+++ js/index.js
@@ -0,0 +1,224 @@
+// ======================================================= Javacript for Almonds ========================================================
+$(function () {
+    $("#quantity").change(function () {
+        var dropdown = document.getElementById("quantity").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price").innerHTML = "Rs 375.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price").innerHTML = "Rs 189.50";
+        } else {
+          document.getElementById("dynamic-price").innerHTML = "Rs 750.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Pista ========================================================
+$(function () {
+    $("#quantity2").change(function () {
+        var dropdown = document.getElementById("quantity2").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price2").innerHTML = "Rs 375.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price2").innerHTML = "Rs 189.50";
+        } else {
+          document.getElementById("dynamic-price2").innerHTML = "Rs 750.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Drydates ========================================================
+$(function () {
+    $("#quantity3").change(function () {
+        var dropdown = document.getElementById("quantity3").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price3").innerHTML = "Rs 375.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price3").innerHTML = "Rs 189.50";
+        } else {
+          document.getElementById("dynamic-price3").innerHTML = "Rs 750.00";
+        }
+    });
+});
+
+
+// ======================================================= Javacript for Kokam Sarbat ========================================================
+$(function () {
+    $("#quantity4").change(function () {
+        var dropdown = document.getElementById("quantity4").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price4").innerHTML = "Rs 125.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price4").innerHTML = "Rs 64.50";
+        } else {
+          document.getElementById("dynamic-price4").innerHTML = "Rs 250.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Malvani masala ========================================================
+$(function () {
+    $("#quantity5").change(function () {
+        var dropdown = document.getElementById("quantity5").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price5").innerHTML = "Rs 325.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price5").innerHTML = "Rs 164.50";
+        } else {
+          document.getElementById("dynamic-price5").innerHTML = "Rs 650.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Big sized polished kaju ========================================================
+$(function () {
+    $("#quantity6").change(function () {
+        var dropdown = document.getElementById("quantity6").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price6").innerHTML = "Rs 475.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price6").innerHTML = "Rs 239.50";
+        } else {
+          document.getElementById("dynamic-price6").innerHTML = "Rs 950.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for peel cashew ========================================================
+$(function () {
+    $("#quantity7").change(function () {
+        var dropdown = document.getElementById("quantity7").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price7").innerHTML = "Rs 345.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price7").innerHTML = "Rs 175.50";
+        } else {
+          document.getElementById("dynamic-price7").innerHTML = "Rs 690.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for salted cashew ========================================================
+$(function () {
+    $("#quantity8").change(function () {
+        var dropdown = document.getElementById("quantity8").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price8").innerHTML = "Rs 345.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price8").innerHTML = "Rs 175.50";
+        } else {
+          document.getElementById("dynamic-price8").innerHTML = "Rs 690.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for green chilie cashew ========================================================
+$(function () {
+    $("#quantity9").change(function () {
+        var dropdown = document.getElementById("quantity9").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price9").innerHTML = "Rs 420.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price9").innerHTML = "Rs 210.00";
+        } else {
+          document.getElementById("dynamic-price9").innerHTML = "Rs 840.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for masala cashew ========================================================
+$(function () {
+    $("#quantity10").change(function () {
+        var dropdown = document.getElementById("quantity10").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price10").innerHTML = "Rs 420.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price10").innerHTML = "Rs 210.00";
+        } else {
+          document.getElementById("dynamic-price10").innerHTML = "Rs 840.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for tukda cashew ========================================================
+$(function () {
+    $("#quantity11").change(function () {
+        var dropdown = document.getElementById("quantity11").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price11").innerHTML = "Rs 345.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price11").innerHTML = "Rs 175.00";
+        } else {
+          document.getElementById("dynamic-price11").innerHTML = "Rs 690.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Yellow Raisins ========================================================
+$(function () {
+    $("#quantity12").change(function () {
+        var dropdown = document.getElementById("quantity12").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price12").innerHTML = "Rs 120.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price12").innerHTML = "Rs 60.00";
+        } else {
+          document.getElementById("dynamic-price12").innerHTML = "Rs 240.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Black Raisins ========================================================
+$(function () {
+    $("#quantity13").change(function () {
+        var dropdown = document.getElementById("quantity13").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price13").innerHTML = "Rs 145.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price13").innerHTML = "Rs 74.50";
+        } else {
+          document.getElementById("dynamic-price13").innerHTML = "Rs 290.00";
+        }
+    });
+});
+
+// ======================================================= Javacript for Brown Raisins ========================================================
+$(function () {
+    $("#quantity14").change(function () {
+        var dropdown = document.getElementById("quantity14").value;
+        if(dropdown == 500)
+        {
+            document.getElementById("dynamic-price14").innerHTML = "Rs 95.00";
+        }
+        else if (dropdown == 250) {
+          document.getElementById("dynamic-price14").innerHTML = "Rs 49.50";
+        } else {
+          document.getElementById("dynamic-price14").innerHTML = "Rs 190.00";
+        }
+    });
+});

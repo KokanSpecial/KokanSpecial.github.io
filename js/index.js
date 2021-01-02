710b2ad6d1c5096a848dde3371c76f117e191661
KokanSpecial <75570203+KokanSpecial@users.noreply.github.com>
2021-01-02 11:33:10 +0530

    Minor price bug fixed

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(function () {
         else if (dropdown == 250) {
           document.getElementById("dynamic-price").innerHTML = "Rs 189.50";
         } else {
-          document.getElementById("dynamic-price").innerHTML = "Rs 750.00";
+          document.getElementById("dynamic-price").innerHTML = "Rs 720.00";
         }
     });
 });
@@ -25,7 +25,7 @@ $(function () {
         else if (dropdown == 250) {
           document.getElementById("dynamic-price2").innerHTML = "Rs 189.50";
         } else {
-          document.getElementById("dynamic-price2").innerHTML = "Rs 750.00";
+          document.getElementById("dynamic-price2").innerHTML = "Rs 720.00";
         }
     });
 });
@@ -41,7 +41,7 @@ $(function () {
         else if (dropdown == 250) {
           document.getElementById("dynamic-price3").innerHTML = "Rs 189.50";
         } else {
-          document.getElementById("dynamic-price3").innerHTML = "Rs 750.00";
+          document.getElementById("dynamic-price3").innerHTML = "Rs 720.00";
         }
     });
 });
